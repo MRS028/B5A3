9683df963c1f86884035e09f29ffa65923c0f80b
Md. Rifat Sheikh <skrifat483@gmail.com>
2025-08-08 12:02:57 +0600

    book.model added

---
+++ src/models/book.model.ts
@@ -0,0 +1,71 @@
+import { Mode } from "fs";
+import mongoose, { Schema, Document, Model } from "mongoose";
+import { title } from "process";
+
+export interface IBook extends Document {
+  title: string;
+  author: string;
+  genre:
+    | "Friction"
+    | "Science"
+    | "Non-Fiction"
+    | "Self-Help"
+    | "Biography"
+    | "Poetry"
+    | "History"
+    | "Drama"
+    | "Philosophy"
+    | "Religion";
+  isbn: string;
+  description?: string;
+  copies: number;
+  available: boolean;
+}
+
+export interface IBookModel extends Model<IBook> {
+  updateAvailability(
+    bookId: mongoose.Types.ObjectId,
+    quantity: number
+  ): Promise<IBook>;
+}
+
+const BookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      "Friction",
+      "Science",
+      "Non-Fiction",
+      "Self-Help",
+      "Biography",
+      "Poetry",
+      "History",
+      "Drama",
+      "Philosophy",
+      "Religion",
+    ],
+  },
+  isbn: { type: String, required: true, unique: true },
+  description: { type: String, required: false },
+  copies: { type: Number, required: true, min: 0 },
+  available: { type: Boolean, default: true },
+});
+
+BookSchema.statics.updateAvailability = async function (
+  bookId: mongoose.Types.ObjectId,
+  quantity: number
+) {
+  const book = await this.findById(bookId);
+  if (!book) {
+    throw new Error("Book not found");
+  }
+  book.copies += quantity;
+  book.available = book.copies > 0;
+  await book.save();
+  return book;
+};  
+
+export const Book = mongoose.model<IBook>("Book", BookSchema);
